d1925d978e55b5a3fee35a59e72060831d1a25a6
Spaghetti Jones <connor.breault@comcast.net>
2019-08-03 10:22:58 -0700

    cards

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -36,9 +36,9 @@ var hotelName = "Hotel Name"
 var hotelDescription = "Hotel info goes here"
 var hotelAddress = "Address goes here"
 
-//append to cards
-for (i = 0; i < 10; i++) {
-    //Card Logic
+//CARD LOGIC
+for (let i = 0; i < 9; i++) {
+
     var newCard = $("<div class='card'>").append(
         newImageDiv,
         newContentDiv,
@@ -46,41 +46,47 @@ for (i = 0; i < 10; i++) {
         newSelector
     )
 
-
+    //IMAGE
+    var newImage = $("<img class='resize'>")
+    newImage.attr("src", "assets/images/circles-and-roundabouts.png.png")
     var newImageDiv = $("<div class='class-image'>").append(
         newImage
     )
-    var newImage = $("<img class='resize'>")
-    newImage.attr("src", "assets/images/circles-and-roundabouts.png.png")
 
+
+    //CONTENT
+    var newContent = $("<span class='card-title activator grey-text text-darken-4'>" + hotelName + "<i class='material-icons right'>more_vert</i>")
 
     var newContentDiv = $("<div class='card-content'>").append(
         newContent,
     )
-    var newContent = $("<span class='card-title activator grey-text text-darken-4'>" + hotelName + "<i class='material-icons right'>more_vert</i>")
 
-
-    var newRevealDiv = $("<div class='card-reveal'>").append(
-        newReveal,
-    )
+    //REVEAL
+    var newRevealParagraph = $("<p class='flow-text'>")
+    newRevealParagraph.append("<br>" + hotelDescription)
     var newReveal = $("<span class='card-title grey-text text-darken-4'>" + hotelName + "<i class='material-icons right'>close</i><br>").append(
         newRevealParagraph
     )
-    var newRevealParagraph = $("<p class='flow-text'>")
-    newRevealParagraph.append("<br>" + hotelDescription)
-
-
-    var newSelector = $("<form action='#' class='center'><p>").append(
-        newSelectorLabel
+    var newRevealDiv = $("<div class='card-reveal'>").append(
+        newReveal,
     )
+
+
+
+
+    //SELECTOR
+    var newSelectorInput = $("<input name='group1' class='selector' type='radio' value='unchecked'>")
+    var newSelectorSpan = $("<span>")
+    newSelectorSpan.text("Select")
     var newSelectorLabel = $("<label>").append(
         newSelectorInput,
         newSelectorSpan
     )
-    var newSelectorInput = $("<input name='group1' class='selector' type='radio' value='unchecked'>")
-    var newSelectorSpan = $("<span>")
-    newSelectorSpan.text("Select")
+    var newSelector = $("<form action='#' class='center'><p>").append(
+        newSelectorLabel
+    )
 
+    //APPEND
     $("#hotelCardDiv").append(
         newCard
     )
