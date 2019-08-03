3b40b0b59e61fe91a8ee0fa2b93b9ee946cdb2fb
Frank P <fpinder@comcast.net>
2019-08-03 11:56:41 -0700

    getting updaates

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -85,7 +85,7 @@ $(document).ready(function () {
     database.ref("/project1").on("child_added", function (snapshot) {
 
         console.log("Project data " + snapshot.val().destination);
-        console.log("date of departure test" + snapshot.val().dateofDeparture);
+        console.log("date of departure " + snapshot.val().dateofDeparture);
 
         //Add data to the table
         var tr = $("<tr>");
@@ -117,64 +117,64 @@ $(document).ready(function () {
 
     })
 
-var hotelName = "Hotel Name"
-var hotelDescription = "Hotel info goes here"
-var hotelAddress = "Address goes here"
+    var hotelName = "Hotel Name"
+    var hotelDescription = "Hotel info goes here"
+    var hotelAddress = "Address goes here"
 
-//CARD LOGIC
-for (let i = 0; i < 9; i++) {
+    //CARD LOGIC
+    for (let i = 0; i < 9; i++) {
 
-    var newCard = $("<div class='card'>").append(
-        newImageDiv,
-        newContentDiv,
-        newRevealDiv,
-        newSelector
-    )
+        var newCard = $("<div class='card'>").append(
+            newImageDiv,
+            newContentDiv,
+            newRevealDiv,
+            newSelector
+        )
 
-    //IMAGE
-    var newImage = $("<img class='resize'>")
-    newImage.attr("src", "assets/images/circles-and-roundabouts.png.png")
-    var newImageDiv = $("<div class='class-image'>").append(
-        newImage
-    )
-
-
-    //CONTENT
-    var newContent = $("<span class='card-title activator grey-text text-darken-4'>" + hotelName + "<i class='material-icons right'>more_vert</i>")
-
-    var newContentDiv = $("<div class='card-content'>").append(
-        newContent,
-    )
-
-    //REVEAL
-    var newRevealParagraph = $("<p class='flow-text'>")
-    newRevealParagraph.append("<br>" + hotelDescription)
-    var newReveal = $("<span class='card-title grey-text text-darken-4'>" + hotelName + "<i class='material-icons right'>close</i><br>").append(
-        newRevealParagraph
-    )
-    var newRevealDiv = $("<div class='card-reveal'>").append(
-        newReveal,
-    )
+        //IMAGE
+        var newImage = $("<img class='resize'>")
+        newImage.attr("src", "assets/images/circles-and-roundabouts.png.png")
+        var newImageDiv = $("<div class='class-image'>").append(
+            newImage
+        )
 
 
+        //CONTENT
+        var newContent = $("<span class='card-title activator grey-text text-darken-4'>" + hotelName + "<i class='material-icons right'>more_vert</i>")
+
+        var newContentDiv = $("<div class='card-content'>").append(
+            newContent,
+        )
+
+        //REVEAL
+        var newRevealParagraph = $("<p class='flow-text'>")
+        newRevealParagraph.append("<br>" + hotelDescription)
+        var newReveal = $("<span class='card-title grey-text text-darken-4'>" + hotelName + "<i class='material-icons right'>close</i><br>").append(
+            newRevealParagraph
+        )
+        var newRevealDiv = $("<div class='card-reveal'>").append(
+            newReveal,
+        )
 
 
-    //SELECTOR
-    var newSelectorInput = $("<input name='group1' class='selector' type='radio' value='unchecked'>")
-    var newSelectorSpan = $("<span>")
-    newSelectorSpan.text("Select")
-    var newSelectorLabel = $("<label>").append(
-        newSelectorInput,
-        newSelectorSpan
-    )
-    var newSelector = $("<form action='#' class='center'><p>").append(
-        newSelectorLabel
-    )
 
-    //APPEND
-    $("#hotelCardDiv").append(
-        newCard
-    )
-}
+
+        //SELECTOR
+        var newSelectorInput = $("<input name='group1' class='selector' type='radio' value='unchecked'>")
+        var newSelectorSpan = $("<span>")
+        newSelectorSpan.text("Select")
+        var newSelectorLabel = $("<label>").append(
+            newSelectorInput,
+            newSelectorSpan
+        )
+        var newSelector = $("<form action='#' class='center'><p>").append(
+            newSelectorLabel
+        )
+
+        //APPEND
+        $("#hotelCardDiv").append(
+            newCard
+        )
+    }
 
 });
